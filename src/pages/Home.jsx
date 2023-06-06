b408f74b4cac1e331f444244dd2dbf98505708e5
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2023-06-06 10:34:34 +0200

    remove alert from homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,6 @@ import Top10 from '../components/charts/Top10';
 import Top10Ads from '../components/charts/Top10Ads';
 import ElectionsCountdown from '../components/general/ElectionsCountdown';
 import TotalSpending from '../components/accounts/TotalSpending';
-import AlertWithIcon from '../components/general/AlertWithIcon';
 import PartiesLogos from '../components/general/PartiesLogos';
 import Title from '../components/structure/Title';
 import Posts, { templates } from '../components/wp/Posts';
@@ -25,21 +24,6 @@ function Home() {
                 <br />
                 voľby
             </Title>
-
-            <AlertWithIcon className="my-4" variant="primary">
-                Vitajte na novej volebnej stránke pre predčasné parlamentné
-                voľby v roku 2023.
-                <br />
-                Volebnú stránku k samosprávnym voľbám 2022 sme presunuli na
-                podstránku{' '}
-                <a
-                    href="https://volby.transparency.sk/samosprava2022/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Samosprávne voľby 2022
-                </a>
-            </AlertWithIcon>
 
             <Row className="gy-3 gy-lg-0 text-center mb-4">
                 <Col lg={6}>
