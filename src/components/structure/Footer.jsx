851ea7462c6c6682ba93608d98a38867f7b75539
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2023-06-08 21:48:24 +0200

    update project donors

--- src/components/structure/Footer.jsx
+++ src/components/structure/Footer.jsx
@@ -11,18 +11,18 @@ import DonateButton from '../general/DonateButton';
 import FbFeed from '../general/FbFeed';
 
 import logoAcf from '../../../public/img/ACF_logo.png';
-import logoEu from '../../../public/img/eu-funded-blue.png';
+// import logoEu from '../../../public/img/eu-funded-blue.png';
 import logoTis from '../../../public/img/tis-logo-blue.png';
-import logoVisegrad from '../../../public/img/Visegrad_logo_black.png';
+// import logoVisegrad from '../../../public/img/Visegrad_logo_black.png';
 
 function Footer() {
     return (
         <footer className="mt-auto">
             <div className="footer-top py-5">
                 <Container>
-                    {/* <h2 className="mb-3 text-center">Donori projektu</h2>
+                    <h2 className="mb-3 text-center">Donori projektu</h2>
                     <Row className="justify-content-around gy-4">
-                        <Col
+                        {/* <Col
                             className="d-flex justify-content-center mb-3 mb-md-0"
                             xs={8}
                             sm={7}
@@ -34,7 +34,7 @@ function Footer() {
                                 className="mw-100 align-self-center"
                                 src={logoVisegrad}
                             />
-                        </Col>
+                        </Col> */}
                         <Col
                             className="d-flex justify-content-center mb-3 mb-md-0"
                             xs={8}
@@ -48,7 +48,7 @@ function Footer() {
                                 src={logoAcf}
                             />
                         </Col>
-                        <Col
+                        {/* <Col
                             className="d-flex justify-content-center mb-3 mb-md-0"
                             xs={8}
                             sm={7}
@@ -64,11 +64,11 @@ function Footer() {
                                     — Police.
                                 </figcaption>
                             </figure>
-                        </Col>
+                        </Col> */}
                     </Row>
                     <Row>
                         <Col>
-                            <p className="donors mt-2 mb-4 fst-italic">
+                            <p className="donors mt-4 mb-4 fst-italic">
                                 Projekt ‘Aktívnym občianstvom ku kvalitnejšej
                                 samospráve (With Active Citizenship for a Better
                                 Selfgovernment)’ je podporený z programu ACF -
@@ -79,7 +79,7 @@ function Footer() {
                                 nadáciou.
                             </p>
                         </Col>
-                    </Row> */}
+                    </Row>
                     <Row>
                         <Col md={6} lg={4}>
                             <h2 className="mb-3">Kontakt</h2>
