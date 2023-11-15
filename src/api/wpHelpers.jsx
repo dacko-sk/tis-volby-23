76ed3507a36b13644b10cae5de1f8a02baa6e7f8
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2023-11-15 15:59:32 +0100

    center images in WP articles

--- src/api/wpHelpers.jsx
+++ src/api/wpHelpers.jsx
@@ -17,6 +17,31 @@ const proxyHttpImages = (html) => {
 
 const parserOptions = {
     replace: ({ name, attribs, children }) => {
+        if (
+            name === 'div' &&
+            attribs &&
+            (attribs.class || '').includes('wp-block-image')
+        ) {
+            // remove "wp-block-image" wrappers, keep just children
+            return domToReact(children, parserOptions);
+        }
+        if (
+            name === 'p' &&
+            attribs &&
+            (attribs.class || '').includes('has-text-align-')
+        ) {
+            // replace paragraph alignment classes from WP with BS5 classes
+            return (
+                <p
+                    className={attribs.class.replace(
+                        /\bhas-text-align-(\w+)\b/g,
+                        'text-$1'
+                    )}
+                >
+                    {domToReact(children, parserOptions)}
+                </p>
+            );
+        }
         if (name === 'img' && attribs && attribs.src) {
             const props = {
                 ...attributesToProps(attribs),
@@ -28,11 +53,6 @@ const parserOptions = {
             return <img {...props} />;
         }
         if (name === 'a') {
-            if (attribs && attribs.rel && attribs.rel.startsWith('lightbox')) {
-                // remove lightbox links
-                // will recursively run parser on children
-                return domToReact(children, parserOptions);
-            }
             if (
                 children.length &&
                 children[0].type === 'text' &&
@@ -41,21 +61,33 @@ const parserOptions = {
                 // remove "continue reading" links to WP domain
                 return <></>;
             }
-            if (attribs && attribs.href.startsWith('http://')) {
-                // fix http links to https
-                const props = {
-                    ...attributesToProps(attribs),
-                    // proxy image to force https
-                    href: attribs.href.replace('http://', 'https://'),
-                };
-                const content = domToReact(children, parserOptions);
-                return <a {...props}>{content}</a>;
+            if (attribs) {
+                if (attribs.rel && attribs.rel.startsWith('lightbox')) {
+                    // remove lightbox links
+                    // will recursively run parser on children
+                    return domToReact(children, parserOptions);
+                }
+                if (attribs.href.startsWith('http://')) {
+                    // fix http links to https
+                    const props = {
+                        ...attributesToProps(attribs),
+                        // proxy image to force https
+                        href: attribs.href.replace('http://', 'https://'),
+                    };
+                    return (
+                        <a {...props}>{domToReact(children, parserOptions)}</a>
+                    );
+                }
             }
         }
         if (name === 'figure') {
-            // add bootstrap 5 classes to figures
+            // add bootstrap 5 classes to figures, remove "wp-block-image" class
             return (
-                <figure className={`figure ${attribs.class || ''}`}>
+                <figure
+                    className={`figure text-center w-100 ${(attribs.class || '')
+                        .replace('wp-block-image', '')
+                        .trim()}`}
+                >
                     {domToReact(children, parserOptions)}
                 </figure>
             );
@@ -63,7 +95,7 @@ const parserOptions = {
         if (name === 'figcaption') {
             // add bootstrap 5 classes to figcaptions
             return (
-                <figcaption className="figure-caption text-center">
+                <figcaption className="figure-caption col-11 col-md-10 col-lg-8 col-xl-6 mx-auto">
                     {domToReact(children, parserOptions)}
                 </figcaption>
             );
